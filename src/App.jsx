a5596721702c8f815fc9f61f9ef12243e9454864
auraexe25 <veenasahu195@gmail.com>
2026-01-20 00:04:24 +0530

    Routing completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import {Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard.jsx'
+import ResumeBuilder from './pages/ResumeBuilder'
+import Login from './pages/Login'
+import Preview from './pages/Preview'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='app' element={<Layout />}>
+        <Route index element={<Dashboard />} />
+        <Route path='builder/:resumeId' element={<ResumeBuilder/>} />
+      </Route>
+      <Route path='view/:resumeId' element={<Preview />} />
+      <Route path='login' element={<Login />} />
+
+
+    </Routes>
+    </>
   )
 }
 
