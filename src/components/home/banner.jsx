6257fa2e13ecd5c5523eafc9b46a1406139ca06b
auraexe25 <veenasahu195@gmail.com>
2026-01-27 10:18:55 +0530

    updated hero section

---
+++ src/components/home/banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const banner = () => {
+  return (
+    
+    <div className="w-full py-2.5 font-medium text-sm text-white text-center bg-linear-to-r from-[#0f0658] to-[#FDFEFF]">
+    <p><span className="px-3 py-1 rounded-lg text-white bg-indigo-600 mr-2">New</span>AI Feature Added</p>
+    </div>
+
+  )
+}
+
+export default banner
